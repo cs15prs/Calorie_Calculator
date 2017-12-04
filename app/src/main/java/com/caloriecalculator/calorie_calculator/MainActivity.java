77ce941cf79c2f85ec79ea5c430e65ed3a630c78
Parth Shah <1539772@my.brunel.ac.uk>
2017-12-04 23:34:59 +0000

    android webview to link calorie calculator website with app

--- app/src/main/java/com/caloriecalculator/calorie_calculator/MainActivity.java
+++ app/src/main/java/com/caloriecalculator/calorie_calculator/MainActivity.java
@@ -2,12 +2,26 @@ package com.caloriecalculator.calorie_calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView mywebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mywebView=(WebView)findViewById(R.id.webview1);
+        WebSettings WebSettings=mywebView.getSettings();
+        WebSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("https://www.verywell.com/recipe-nutrition-analyzer-4129594");
+        mywebView.setWebViewClient(new WebViewClient());
     }
 }
--- app/src/main/java/com/caloriecalculator/calorie_calculator/SplashActivity.java
+++ app/src/main/java/com/caloriecalculator/calorie_calculator/SplashActivity.java
@@ -1,13 +1,35 @@
 package com.caloriecalculator.calorie_calculator;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }

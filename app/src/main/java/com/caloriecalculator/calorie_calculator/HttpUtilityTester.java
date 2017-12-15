5cb2d9ab030d09aee4ba1c18b68dee443cc21177
Parth Shah <1539772@my.brunel.ac.uk>
2017-12-15 08:34:24 +0000

    Fixed HTTP Utility Error

--- app/src/main/java/com/caloriecalculator/calorie_calculator/HttpUtilityTester.java
+++ app/src/main/java/com/caloriecalculator/calorie_calculator/HttpUtilityTester.java
@@ -10,50 +10,201 @@ import android.view.View;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.TextView;
 
 public class HttpUtilityTester extends AppCompatActivity {
-
+    private ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_http_utility_tester);
-        // test sending GET request
-        String requestURL = "http://www.google.com";
-        try {
-            HttpUtility.sendGetRequest(requestURL);
-            String[] response = HttpUtility.readMultipleLinesRespone();
-            for (String line : response) {
-                System.out.println(line);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        HttpUtility.disconnect();
 
-
-        System.out.println("=====================================");
-
-        // test sending POST request
-        Map<String, String> params = new HashMap<String, String>();
-        requestURL = "https://accounts.google.com/ServiceLoginAuth";
-        params.put("Email", "your_email");
-        params.put("Passwd", "your_password");
-
-        try {
-            HttpUtility.sendPostRequest(requestURL, params);
-            String[] response = HttpUtility.readMultipleLinesRespone();
-            for (String line : response) {
-                System.out.println(line);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        HttpUtility.disconnect();
-    }
 
     }
+    public void sendPostRequest(View View) {
+        new PostClass(this).execute();
+    }
+
+    public void sendGetRequest(View View) {
+        new GetClass(this).execute();
+    }
+
+    private class PostClass extends AsyncTask<String, Void, Void> {
+
+        private final Context context;
+
+        public PostClass(Context c){
+
+            this.context = c;
+//            this.error = status;
+//            this.type = t;
+        }
+
+        protected void onPreExecute(){
+            progress= new ProgressDialog(this.context);
+            progress.setMessage("Loading");
+            progress.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+
+                final TextView outputView = (TextView) findViewById(R.id.showOutput);
+                URL url = new URL("https://www.verywell.com/recipe-nutrition-analyzer-4129594");
+
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                String urlParameters = "parameter=value&also=another";
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+                // connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+                connection.setDoOutput(true);
+                DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+                dStream.writeBytes(urlParameters);
+                dStream.flush();
+                dStream.close();
+                int responseCode = 443;
+
+                System.out.println("\nSending 'POST' request to URL : " + url);
+                System.out.println("Post parameters : " + urlParameters);
+                System.out.println("Response Code : " + responseCode);
+
+                final StringBuilder output = new StringBuilder("Request URL " + url);
+                output.append(System.getProperty("line.separator") + "Request Parameters " + urlParameters);
+                output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
+                output.append(System.getProperty("line.separator")  + "Type " + "POST");
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+                System.out.println("output===============" + br);
+                while((line = br.readLine()) != null ) {
+                    responseOutput.append(line);
+                }
+                br.close();
+
+                output.append(System.getProperty("line.separator") + "Response " + System.getProperty("line.separator") + System.getProperty("line.separator") + responseOutput.toString());
+
+                HttpUtilityTester.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        outputView.setText(output);
+                        progress.dismiss();
+                    }
+                });
+
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute() {
+            progress.dismiss();
+        }
+
+    }
+
+    private class GetClass extends AsyncTask<String, Void, Void> {
+
+        private final Context context;
+
+        public GetClass(Context c){
+            this.context = c;
+        }
+
+        protected void onPreExecute(){
+            progress= new ProgressDialog(this.context);
+            progress.setMessage("Loading");
+            progress.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+
+                final TextView outputView = (TextView) findViewById(R.id.showOutput);
+                URL url = new URL("http://requestb.in/1cs29cy1");
+
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                String urlParameters = "fizz=buzz";
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+                connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+
+                int responseCode = connection.getResponseCode();
+
+                System.out.println("\nSending 'POST' request to URL : " + url);
+                System.out.println("Post parameters : " + urlParameters);
+                System.out.println("Response Code : " + responseCode);
+
+                final StringBuilder output = new StringBuilder("Request URL " + url);
+                //output.append(System.getProperty("line.separator") + "Request Parameters " + urlParameters);
+                output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
+                output.append(System.getProperty("line.separator")  + "Type " + "GET");
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+                System.out.println("output===============" + br);
+                while((line = br.readLine()) != null ) {
+                    responseOutput.append(line);
+                }
+                br.close();
+
+                output.append(System.getProperty("line.separator") + "Response " + System.getProperty("line.separator") + System.getProperty("line.separator") + responseOutput.toString());
+
+                HttpUtilityTester.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        outputView.setText(output);
+                        progress.dismiss();
+
+                    }
+                });
+
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+//        protected void onPostExecute() {
+//            progress.dismiss();
+//        }
+
+    }
+
+}
 
 
 
